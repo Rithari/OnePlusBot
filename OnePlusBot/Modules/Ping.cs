428fbf8fea7a3514f9619d80ebe7d310de44ed45
Rithari <rithari@outlook.com>
2018-10-12 21:45:23 +0200

    Added two commands.
    Global:
    - Added ping command;
    - Added fun "skid level" command.
    
    Linux:
    - Built to latest for linux

---
+++ OnePlusBot/Modules/Ping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace OnePlusBot.Modules
+{
+    public class PingModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            var timestamp = Context.Message.Timestamp;
+            var ping = timestamp - DateTime.UtcNow;
+
+            var sentmessage = await ReplyAsync("Pong!");
+            await sentmessage.ModifyAsync(m => m.Content = "Pong....\nIn " + ping.Milliseconds + " ms");
+        }
+    }
+}
---
+++ OnePlusBot/Modules/Skid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using Discord;
+using System.Threading.Tasks;
+
+namespace OnePlusBot.Modules
+{
+    public class SkidModule : ModuleBase<SocketCommandContext>
+    {
+        public async Task SkidAsync([Remainder] string user)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 100);
+
+            await ReplyAsync(user + "'s skid level is: " + randomNumber + "%");
+        }
+    }
+}
