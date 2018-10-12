9bd95ea3239ac47f110c029719ec7f830c0beaee
Rithari <rithari@outlook.com>
2018-10-12 21:39:09 +0200

    Added shutdown.
    Global:
    - Added shutdown for emergency situations.
    
    Linux:
    - Built to latest for Linux. (this time for real)

---
+++ OnePlusBot/Modules/Shutdown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+namespace OnePlusBot.Modules
+{
+    public class ShutdownModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("x")]
+        [Summary("Emergency shutdown of the bot.")]
+        [RequireOwner]
+        public async Task ShutdownAsync()
+        {
+            var EmoteTrue = new Emoji(":success:499567039451758603");
+            await Context.Message.AddReactionAsync(EmoteTrue);
+            Environment.Exit(0);
+        }
+
+    }
+}
