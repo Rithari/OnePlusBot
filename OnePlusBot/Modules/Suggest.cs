dfb7fec82aa6cd5f08acd89bfff3cb21d61d7264
Rithari <rithari@outlook.com>
2018-10-17 16:35:20 +0200

    Added suggest command.
    Global:
    - Added suggest command.
    
    Linux:
    - Built to latest for Linux.

---
+++ OnePlusBot/Modules/Suggest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using System.Linq;
+
+namespace OnePlusBot.Modules
+{
+    public class SuggestModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("suggest")]
+        [Summary("Suggests something to the server.")]
+        public async Task SuggestAsync([Remainder] string suggestion)
+        {
+            var channels = Context.Guild.TextChannels;
+            var suggestionschannel = channels.FirstOrDefault(x => x.Name == "suggestions");
+
+            var user = Context.Message.Author; 
+
+            await suggestionschannel.SendMessageAsync(suggestion + "\n **Suggested by**: " + user.Mention);
+
+            await Context.Message.DeleteAsync();
+
+           
+
+
+         }
+    }
+}
