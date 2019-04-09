fc3aac7d97f1cad6bd4b2eadd85df044eda038b6
Lavin Amarnani <49375935+Misturtroll@users.noreply.github.com>
2019-04-09 21:50:43 +0530

    Fixed Love.cs
    Command can now be used with or without "and" between parameters, and @mentions will work fine now.

--- src/OnePlusBot/Modules/Love.cs
+++ src/OnePlusBot/Modules/Love.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
 using System.Text.RegularExpressions;
 using System.Net;
+using Discord;
 
 namespace OnePlusBot.Modules
 {
@@ -12,14 +13,21 @@ namespace OnePlusBot.Modules
     {
         [Command("lovecalc")]
         [Summary("lovecalc search for Discord!")]
-        public async Task LMGTFYAsync([Remainder] string parameter)
+        public async Task LoveCalcAsync([Remainder] string parameter)
         {
-            string cacheWords = Regex.Split(" " + Context.Message.Content, @"\;lovecalc\s+")[1];
-            string[] words = Regex.Split(cacheWords, @"\s+and\s+|\,\s+");
-            if(words[0] == "" || words[0] == null || words[1] == "" || words[1] == null) {await ReplyAsync("Usage: ;lovecalc name1 and name2");}
-            if(words[0].Contains('#')) {words[0]=words[0].Substring(0, words[0].IndexOf('#'));}
-            if(words[1].Contains('#')) {words[1]=words[1].Substring(0, words[1].IndexOf('#'));}
-            cacheWords = new WebClient().DownloadString("https://www.lovecalculator.com/love.php?name1=" + words[0] + "&name2=" + words[1]);
+            string first, second, cacheWords = Regex.Split(" " + Context.Message.Content, @"\;lovecalc\s+")[1];
+            string[] words;
+            if (cacheWords.Contains("and")) { words = Regex.Split(cacheWords, @"\s+and\s+|\,\s+"); }
+            else { words = Regex.Split(cacheWords, @"\s+"); }
+            first = Regex.Replace(words[0], "[^0-9a-zA-Z]", "");
+            second = Regex.Replace(words[1], "[^0-9a-zA-Z]", "");
+            await ReplyAsync(first + " " + second);
+            if(words[0].Contains("@"))
+            first = Context.Guild.GetUser(ulong.Parse(first)).Username;
+            if(words[1].Contains("@"))
+            second = Context.Guild.GetUser(ulong.Parse(second)).Username;
+            await ReplyAsync(first + " " + second);
+            cacheWords = new WebClient().DownloadString("https://www.lovecalculator.com/love.php?name1=" + first + "&name2=" + second);
             await ReplyAsync(":cupid: Love chance between " + words[0] + " and " + words[1] + ": `" + Regex.Split(Regex.Split(cacheWords, @"\<div\s+class\=.*?result\s+score.*?\>")[1], @"\<\/div\>")[0] + "`");
         }
     }
