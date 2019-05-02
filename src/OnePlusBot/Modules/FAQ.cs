185f7a5599927a66ee9e87bca10ff4d6584cc93b
KB4284848 <42769194+KB4284848@users.noreply.github.com>
2019-05-03 00:19:01 +0200

    Update FAQ.cs
    Add info about edl mode in FAQ

--- src/OnePlusBot/Modules/FAQ.cs
+++ src/OnePlusBot/Modules/FAQ.cs
@@ -156,10 +156,16 @@ namespace OnePlusBot.Modules
                             await ReplyAsync("For all device APKs, please visit: <https://www.celsoazevedo.com/files/android/google-camera>");
                         }
                         break;
-
-
-                    default:
-                        await ReplyAsync("Supported commands are: bluspark, googlecamera, oxygenos, unbrick, magisk, root");
+                    case "edl":
+                    case "9008":
+                    case "9008mode":
+                        {
+                            await ReplyAsync("To enter in Qualcomm EDL mode, you can use `adb reboot edl` or use any version of blu_spark TWRP based on TWRP 3.3.0 or later by clicking on `Reboot to EDL`.>" + Environment.NewLine +
+                                             "If you want to exit EDL mode, maintain power button during at least 10 seconds ");
+                        }
+                        break;
+                            default:
+                        await ReplyAsync("Supported commands are: bluspark, googlecamera, oxygenos, unbrick, magisk, root, edl");
                         break;
                 }
             }
