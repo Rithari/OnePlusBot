9946b3854f9208ac154d00ddf53dfec31bf17042
The Oddball <oddball@oddball.tf>
2019-05-05 14:10:29 -0700

    Add channel reply on ban

--- src/OnePlusBot/Modules/Ban.cs
+++ src/OnePlusBot/Modules/Ban.cs
@@ -40,13 +40,18 @@ namespace OnePlusBot.Modules
             {
                 var EmoteTrue = new Emoji(":success:499567039451758603");
                 await Context.Message.AddReactionAsync(EmoteTrue);
+
+                await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005)
+                .WithTitle("⛔️ Banned User")
+                .AddField(efb => efb.WithName("Username").WithValue(user.ToString()).WithIsInline(true))
+                .AddField(efb => efb.WithName("ID").WithValue(user.Id.ToString()).WithIsInline(true)));
+
                 await user.SendMessageAsync("You were banned on r/OnePlus for the following reason: " + reason + "\nIf you believe this to be a mistake, please send an appeal e-mail with all the details to admin@kyot.me");
                 await Context.Guild.AddBanAsync(user, 0, reason);
 
             }
             catch (Exception)
             {
-                await Context.Guild.AddBanAsync(user, 0, reason);
             }
         }
     }
