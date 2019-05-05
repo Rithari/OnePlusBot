d52e8a147a6352de437035d54858d7fc30dd96c4
Rithari <rithari@outlook.com>
2019-05-05 20:10:23 +0200

    Better emotes for joinable reactions.

--- src/OnePlusBot/Modules/BuildReactTo.cs
+++ src/OnePlusBot/Modules/BuildReactTo.cs
@@ -18,21 +18,31 @@ namespace OnePlusBot.Modules
         {
             RestUserMessage reactmsg = await Context.Channel.SendMessageAsync("**Self Assignable Roles**" +
                 "\n\n__You can assign yourself one of the following roles by reacting to its corresponding emote.__\n" +
-                "\n OnePlus One - \U00000030\U000020e3 " +
-                "\n OnePlus 2 - \U00000031\U000020e3" +
-                "\n OnePlus X - \U00000032\U000020e3" +
-                "\n OnePlus 3 - \U00000033\U000020e3" +
-                "\n OnePlus 3T - \U00000034\U000020e3" +
-                "\n OnePlus 5 - \U00000035\U000020e3" +
-                "\n OnePlus 5T - \U00000036\U000020e3" +
-                "\n OnePlus 6 - \U00000037\U000020e3" +
-                "\n OnePlus 6T - \U00000038\U000020e3" +
-                "\n Helper - ❓" +
-                "\n News - 📰" +
+                "\n OnePlus One" +
+                "\n OnePlus 2" +
+                "\n OnePlus X" +
+                "\n OnePlus 3" +
+                "\n OnePlus 3T" +
+                "\n OnePlus 5" +
+                "\n OnePlus 5T" +
+                "\n OnePlus 6" +
+                "\n OnePlus 6T" +
+                "\n Helper" +
+                "\n News" +
                 "\n\n *Leaving a role you have joined is currently unsupported and wil be added soon, for now, please contact administration.*");
 
             Global.ReactBuilderMsgId = reactmsg.Id;
-            await reactmsg.AddReactionsAsync(new Emoji[] {new Emoji("\U00000030\U000020e3"), new Emoji("\U00000031\U000020e3"), new Emoji("\U00000032\U000020e3"), new Emoji("\U00000033\U000020e3"), new Emoji("\U00000034\U000020e3"), new Emoji("\U00000035\U000020e3"), new Emoji("\U00000036\U000020e3"), new Emoji("\U00000037\U000020e3"), new Emoji("\U00000038\U000020e3"), new Emoji("❓"), new Emoji("📰") });
+            await reactmsg.AddReactionsAsync(new Emoji[] 
+              { new Emoji(":1_:574655515586592769"),
+                new Emoji(":2_:574655515548844073"),
+                new Emoji(":X_:574655515481866251"),
+                new Emoji(":3_:574655515452506132"),
+                new Emoji(":3T:574655515846508554"),
+                new Emoji(":5_:574655515745976340"),
+                new Emoji(":5T:574655515494318109"),
+                new Emoji(":6_:574655515615952896"),
+                new Emoji(":6T:574655515846508573"),
+                new Emoji("❓"), new Emoji("📰") });
 
         }
 
