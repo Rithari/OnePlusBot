a38fb44844bb819a99c7ff767a73647df334ba34
The Oddball <oddball@oddball.tf>
2019-05-19 09:37:12 -0700

    Make news only work if sent in the news channel

--- src/OnePlusBot/Modules/News.cs
+++ src/OnePlusBot/Modules/News.cs
@@ -11,8 +11,6 @@ namespace OnePlusBot.Modules
     {
         [Command("news")]
         [Summary("Posts a News article to the server.")]
-        [RequireUserPermission(GuildPermission.PrioritySpeaker)]
-        [RequireUserPermission(GuildPermission.ManageNicknames)]
         public async Task NewsAsync([Remainder] string news)
         {
             var channels = Context.Guild.TextChannels;
@@ -20,21 +18,23 @@ namespace OnePlusBot.Modules
             var role = Context.Guild.Roles.FirstOrDefault(x => x.Name == "News");
 
             var user = Context.Message.Author;
-
-            if (news.Contains("@everyone") || news.Contains("@here") || news.Contains("@news"))
+            if (Context.Channel.Name == "news")
             {
-                await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithDescription("⚠ That news contains an illegal ping! Don't do that!"));
-                return;
+                if (news.Contains("@everyone") || news.Contains("@here") || news.Contains("@news"))
+                {
+                    await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithDescription("⚠ That news contains an illegal ping! Don't do that!"));
+                    return;
+                }
+
+
+                await role.ModifyAsync(x => x.Mentionable = true);
+
+                await newschannel.SendMessageAsync(news + Environment.NewLine + Environment.NewLine + role.Mention + Environment.NewLine + "- " + user);
+
+                await role.ModifyAsync(x => x.Mentionable = false);
+
+                await Context.Message.DeleteAsync();
             }
-
-
-            await role.ModifyAsync(x => x.Mentionable = true);
-
-            await newschannel.SendMessageAsync(news + Environment.NewLine + Environment.NewLine + role.Mention + Environment.NewLine + "- " + user);
-
-            await role.ModifyAsync(x => x.Mentionable = false);
-
-            await Context.Message.DeleteAsync();
         }
     }
 }
