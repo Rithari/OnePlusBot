4e49047f6a7a5f430dde833524fdba1bd6c60549
The Oddball <oddball@oddball.tf>
2019-05-21 11:29:51 -0700

    Add showemoji command

---
+++ src/OnePlusBot/Modules/showemoji.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using OnePlusBot._Extensions;
+using Discord.Commands;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace OnePlusBot.Modules
+{
+    public class ShowEmoji : ModuleBase<SocketCommandContext>
+    {
+        [Command("se")]
+        [Summary("Shows avatar of a user.")]
+        public async Task Showemojis([Remainder] string _) // need to have the parameter so that the message.tags gets populated
+        {
+            var tags = Context.Message.Tags.Where(t => t.Type == TagType.Emoji).Select(t => (Emote)t.Value);
+            
+            var result = string.Join("\n", tags.Select(m => "**Name:** " + m + " **Link:** " + m.Url));
+
+            if (string.IsNullOrWhiteSpace(result))
+                await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithDescription("No special emojis found."));
+            else
+                await Context.Channel.SendMessageAsync(result).ConfigureAwait(false);
+        }
+    }
+}
