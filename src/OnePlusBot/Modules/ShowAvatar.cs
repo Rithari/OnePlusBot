07d38a928aeea1a9fb89b5713be6f1d66f5f5f82
The Oddball <oddball@oddball.tf>
2019-05-21 11:15:34 -0700

    Add showavatar command

---
+++ src/OnePlusBot/Modules/ShowAvatar.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using OnePlusBot._Extensions;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace OnePlusBot
+{
+    public class ShowAvatar : ModuleBase<SocketCommandContext>
+    {
+        [Command("showavatar")]
+        [Summary("Shows avatar of a user.")]
+        public async Task Avatar(IGuildUser usr = null)
+        {
+            if (usr == null)
+                usr = (IGuildUser)Context.User;
+
+            var avatarUrl = usr.RealAvatarUrl();
+
+            if (avatarUrl == null)
+            {
+                await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithDescription("User has no avatar."));
+                return;
+            }
+
+            await Context.Channel.EmbedAsync(new EmbedBuilder().WithColor(9896005)
+                .AddField(efb => efb.WithName("Username").WithValue(usr.ToString()).WithIsInline(false))
+                .AddField(efb => efb.WithName("Avatar Url").WithValue(avatarUrl).WithIsInline(false))
+                .WithThumbnailUrl(avatarUrl.ToString())
+                .WithImageUrl(avatarUrl.ToString()));
+        }
+    }
+}
