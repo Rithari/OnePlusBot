8618273220641585ef1c0f1dd5576b1aaef67c47
Keanu <guestone63@gmail.com>
2019-05-28 16:06:19 +0100

    Tidied up suggestions embed, added title parameter
    Gotta take advantage of Discord's beauty.

--- src/OnePlusBot/Modules/Suggest.cs
+++ src/OnePlusBot/Modules/Suggest.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using Discord;
 using Discord.Commands;
 using System.Linq;
@@ -10,17 +11,17 @@ namespace OnePlusBot.Modules
     {
         [Command("suggest")]
         [Summary("Suggests something to the server.")]
-        public async Task SuggestAsync([Remainder] string suggestion)
+        public async Task SuggestAsync([Remainder] string title, string suggestion)
         {
             var channels = Context.Guild.TextChannels;
             var suggestionschannel = channels.FirstOrDefault(x => x.Name == "suggestions");
 
             var user = Context.Message.Author;
+            var userpfp = RealAvatarUrl(Context.Message.Author);
 
-            if (suggestion.Contains("@everyone") || suggestion.Contains("@here"))
-                return;
+            if (suggestion.Contains("@everyone") || suggestion.Contains("@here")) suggestion = suggestion.Replace(suggestion, @"@(everyone|here)", "")
 
-            var oldmessage = await suggestionschannel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithDescription(suggestion).WithFooter("" + user));
+            var oldmessage = await suggestionschannel.EmbedAsync(new EmbedBuilder().WithColor(9896005).WithTitle(title).WithDescription(suggestion).WithAuthor(user, userpfp).WithFooter("Made by Rithari#0001", RealAvatarUrl(Context.Client.CurrentUser)).WithCurrentTimestamp);
 
             await oldmessage.AddReactionsAsync(new Emoji[] { new Emoji(":OPYes:426070836269678614"), new Emoji(":OPNo:426072515094380555") });
             await Context.Message.DeleteAsync();
