5d96be3c900aae210f5d3ac73b242099d7db827c
Sheldan <5037282+Sheldan@users.noreply.github.com>
2019-10-07 10:14:11 +0200

    added remind command (#143)
    * added remind command, fixes #133
    
    * minor refactoring

---
+++ src/OnePlusBot/Data/Models/Reminder.cs
@@ -0,0 +1,38 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Discord;
+
+namespace OnePlusBot.Data.Models
+{
+    [Table("Reminders")]
+    public class Reminder
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint ID { get; set; }
+        
+        [Column("reminded_user_id")]
+        public ulong RemindedUserId { get; set; }
+
+
+        [Column("remind_text")]
+        public string RemindText { get; set; }
+
+        [Column("reminder_date")]
+        public DateTime ReminderDate { get; set; }
+        
+        [Column("target_date")]
+        public DateTime TargetDate { get; set; }
+
+        [Column("reminded")]
+        public Boolean Reminded {get; set; }
+
+        [Column("channel_id")]
+        public ulong ChannelId { get; set; }
+
+        [Column("message_id")]
+        public ulong MessageId { get; set; }
+    }
+}
